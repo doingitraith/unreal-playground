86cb0e0afbae57e69a52301d7ca7355dfd48f2eb
Jakob Raith <raith.jakob@outlook.com>
2023-10-14 17:04:57 +0200

    setup project

---
+++ Source/UnrealPlayground/UnrealPlaygroundGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealPlaygroundGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUnrealPlaygroundGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUnrealPlaygroundGameMode();
+};
+
+
+
